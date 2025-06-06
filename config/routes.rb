5eb9b124b5234878f648b7c0b5db68138b7d804a
Cacha9612 <oswaldoaraelg@gmail.com>
2025-06-06 17:53:19 -0600

    modificaciones

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Ruta raíz que responde algo simple para verificar que la API está viva
+  root to: proc { [200, {}, ['API funcionando']] }
+
   # Ruta para buscar un empleado por su campo IdUsuario
   get 'empleados/por_usuario/:id', to: 'empleados#por_usuario'
 
