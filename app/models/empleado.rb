1371aaa11f25e134ef10a3b3b0483c6dd5864b26
Cacha9612 <oswaldoaraelg@gmail.com>
2025-06-06 22:10:41 -0600

    modificaciones

--- app/models/empleado.rb
+++ app/models/empleado.rb
@@ -1,5 +1,5 @@
 class Empleado < ApplicationRecord
-  enum Rol: { 1: 1, 2: 2, 4: 3, 4: 4 }
+  enum Rol: { Administrador:1 , Almacén: 2, Técnicos: 3, Jefé de Taller: 4 }
   enum Estatus: { Inactivo: 0, Activo: 1 }
 
   validates :IdUsuario, presence: true, uniqueness: true
