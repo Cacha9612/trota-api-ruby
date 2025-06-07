57f8b8a2fdc5e56c1bbf1c8dbc406cf6ba5da816
Cacha9612 <oswaldoaraelg@gmail.com>
2025-06-06 23:08:32 -0600

    modificaciones

--- app/models/empleado.rb
+++ app/models/empleado.rb
@@ -1,6 +1,6 @@
 class Empleado < ApplicationRecord
-  belongs_to :role, foreign_key: 'Id_Rol'
-
+  
+  belongs_to :role, foreign_key: 'Id_Rol', class_name: 'Role' 
   enum Estatus: { inactivo: 0, activo: 1 }
 
   validates :IdUsuario, presence: true, uniqueness: true
